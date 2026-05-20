ee4207ea8835f6adf93835578c8f0375ca131f5b
writercodes1 <138004163+writercodes1@users.noreply.github.com>
2026-05-20 17:04:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from '@tailwindcss/vite'
-import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwind(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  assetsInclude: ['**/*.svg', '**/*.csv'],
+  plugins: [react()],
 })
