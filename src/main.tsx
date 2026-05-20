244238a42f423ab5b8065d88fbc5b29ba87acaac
writercodes1 <138004163+writercodes1@users.noreply.github.com>
2026-05-20 17:41:41 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // @ts-ignore: side-effect import for global CSS
-import './styles/fonts.css'
+//import './styles/fonts.css'
 // @ts-ignore: side-effect import for global CSS
-import './styles/theme.css'
+//import './styles/theme.css'
 // @ts-ignore: side-effect import for global CSS
 import './index.css'
 import App from './app/App.tsx'
